9d524a1775010340958d123b037ee6a028422641
Firuddin Kərimli <firuddin2006@gmail.com>
2026-08-11 00:09:23 +0400

    Delete SQL LAYIHE(3).sql

--- SQL LAYIHE(3).sql
+++
@@ -1,9 +0,0 @@
-﻿--2.
---Excel Table-dəki dataları Oracle-a import edib aşağıdakı tapşırıqları həll edin.
---Hər bir table-da Constraintlərdən ən azı 1-i  təyin olunmalıdır.
-
-SELECT * FROM tb_rating_info FOR UPDATE;
-SELECT COUNT(*) FROM tb_rating_info ;
-
-SELECT * FROM tb_imdb_movies FOR UPDATE;
-SELECT COUNT(*) FROM tb_imdb_movies
